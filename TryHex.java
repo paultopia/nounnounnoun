7afbd532271f3d6e4f8dbee6f1eda4f21422bce4
Paul Gowder <paul.gowder@gmail.com>
2017-08-17 21:46:14 -0500

    all the hex things

--- TryHex.java
+++ TryHex.java
@@ -2,7 +2,7 @@ import cryptopals.Hex;
 
 public class TryHex{
     public static void main(String[] args){
-        Hex hex = new Hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
-        hex.printbytes();
+        Hex hex = new Hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"); // cryptopals 1
+        hex.showoff();
     }
 }
--- cryptopals/Hex.java
+++ cryptopals/Hex.java
@@ -2,18 +2,25 @@ package cryptopals;
 
 import javax.xml.bind.DatatypeConverter;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class Hex {
     private String hex_str;
     private byte[] byte_array;
     private String decoded_string;
     private String byte_array_string;
+    private byte[] base_64_byte_array;
+    private String base_64_byte_array_string;
+    private String base_64_string;
 
     public Hex(String myhex){
         hex_str = myhex;
         byte_array = DatatypeConverter.parseHexBinary(hex_str);
         byte_array_string = Arrays.toString(byte_array);
         decoded_string = new String(byte_array);
+        base_64_byte_array = Base64.getEncoder().encode(byte_array);
+        base_64_byte_array_string = Arrays.toString(base_64_byte_array);
+        base_64_string = new String(base_64_byte_array);
 
     }
 
@@ -21,9 +28,12 @@ public class Hex {
         return hex_str;
     }
 
-    public void printbytes(){
-        System.out.println(byte_array_string);
-        System.out.println(decoded_string);
+    public void showoff(){
+        System.out.println("\nThe original hex string is:\n\n" + hex_str);
+        System.out.println("\nHere it is, converted to a byte array:\n\n" + byte_array_string);
+        System.out.println("\nHere it is in ascii form:\n\n" + decoded_string);
+        System.out.println("\nHere it is converted to bytes again, but after a run through base 64:\n\n" + base_64_byte_array_string);
+        System.out.println("\nAnd here's a base 64 string:\n\n" + base_64_string);
 
     }
 }
