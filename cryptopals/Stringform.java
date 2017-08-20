bde987d7da357351423c4c5a1693bb6bd4ff1db9
Paul Gowder <paultopia@users.noreply.github.com>
2017-08-20 13:43:55 -0500

    first stab at uneven xor, stringform still not tested

--- cryptopals/Stringform.java
+++ cryptopals/Stringform.java
@@ -59,10 +59,37 @@ public class Stringform {
 		return hex;
 	}
 	
-	public Stringform xor(Stringform other){ // this will need to be branched for case of different lengths.  to handle cryptopals 5, I'll just recycle the smaller one.  but first this new class needs to be tested with existing solved challenges. Then I'll just dispatch uneven cases to a separate function.
-		int size = bytes.length;
+	private Stringform unevenXOR(byte[] other_bytes){ // assumes I want to rotate shorter string around to xor longer one, NOT discard unused bits of longer one (which roughly corresponds to the idea that the plaintext will always be longer than the key).  If this assumption becomes false then I'll have to rewrite this, or perhaps just change what gets called from the public xor method below.
+		byte[] longer;
+		byte[] shorter;
+		if (bytes.length > other_bytes.length){
+			longer = bytes;
+			shorter = other_bytes;
+		} else {
+			longer = other_bytes;
+			shorter = bytes;
+		}
+		int size = longer.length;
 		byte[] result = new byte[size];
+		int limit = shorter.length;
+		int rot = 0;
+		for (int i = 0; i < size; i++){
+            result[i] = (byte)(longer[i] ^ shorter[rot]);
+            rot ++;
+            if (rot == limit){
+            	rot = 0;
+            }
+        }
+        return new Stringform(result);
+	}
+	
+	public Stringform xor(Stringform other){
+		int size = bytes.length;
         byte[] other_bytes = other.getBytes();
+        if (size != other_bytes.length) {
+        	return unevenXOR(other_bytes);
+        }
+		byte[] result = new byte[size];
         for (int i = 0; i < size; i++){
             result[i] = (byte)(bytes[i] ^ other_bytes[i]);
         }
