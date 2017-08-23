3f916a6833bc906d77acd86d93d475c191b19f71
Paul Gowder <paul.gowder@gmail.com>
2017-08-23 12:30:15 -0500

    use try with resources statement in io, plus get rid of unnecessary imports

--- cryptopals/src/main/java/Lines.java
+++ cryptopals/src/main/java/Lines.java
@@ -1,20 +1,16 @@
 package cryptopals;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.io.*;
-import java.util.Scanner;
 
 
 public class Lines{
     public static List<String> getLines(String filename){
         List<String> lines = new ArrayList<String>();
         String line;
-        try {
-            InputStream in = Lines.class.getResourceAsStream(filename); // note to self: this requires directory with slash, or plain slash if in root directory, appended to filename.  Ref: https://stackoverflow.com/questions/14739550/difference-between-getclass-getclassloader-getresource-and-getclass-getres
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        try (InputStream in = Lines.class.getResourceAsStream(filename); 
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in));) {
                 while ((line = reader.readLine()) != null) {
                     lines.add(line);
                 }
@@ -25,3 +21,5 @@ public class Lines{
         return lines;
     }
 }
+
+// note to self: class.getResourceAsStream() requires directory with slash, or plain slash if in root directory, appended to filename. Ref: https://stackoverflow.com/questions/14739550/difference-between-getclass-getclassloader-getresource-and-getclass-getres And it would be getClas().getResourceAsStream() outside a static method I think?
